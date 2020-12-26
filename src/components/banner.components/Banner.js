aaac5b34da78df704018771f5292a461cc9399f5
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2020-12-26 01:37:41 +0100

    styles edited in Banner.js

--- src/components/banner.components/Banner.js
+++ src/components/banner.components/Banner.js
@@ -1,34 +1,33 @@
 import React from 'react';
+import {Container} from "react-bootstrap";
 import bannerImage from "../../images/banner.png";
 import webDev from "../../images/webDev.jpg";
 import "./banner.styles.css";
 
 function Banner() {
     return (
-        <>
-        <div className="bannerSection">
-            <img src={bannerImage} alt=""/>
-        </div>
-        <div className="mainSection">
-            <div className="weDevelop">
+        <div className="heroSection">
+            <div className="bannerSection">
+                <img src={bannerImage} alt=""/>
+            </div>
+            <div className="mainSection">
                 <div className="imageContainer">
                     <img src={webDev} alt=""/>
                     <div className="content">
                         <h2>We Develop</h2>
-                        <p className="weFocus">We focus on quality & Cost-Effective solution</p>
+                        <p className="weFocus">We focus on quality & 
+                            Cost-Effective solution</p>
                     </div>
                 </div>
-                <div className="getQuoteBtn">
-                    <h1>WEB DEVELOPMENT</h1>
-                    {/* <p className="weFocus">We focus on quality & Cost-Effective solution</p> */}
-                    <p className="devTeam">Find a development team to get the best 
-                        development solutions
+                <div className="getQuoteSection">
+                    <h2>WEB DEVELOPMENT</h2>
+                    <p className="devTeam">Find a development team to 
+                        get the best development solutions
                     </p>
                     <button>Get a Quote</button>
                 </div>
             </div>
         </div>
-        </>
     )
 }
 
