1cbef31f7c4a88f3999764311babee38c395587c
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2020-12-29 00:46:04 +0100

    about section developed

--- src/pages/about.pages/About.js
+++ src/pages/about.pages/About.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Container} from "react-bootstrap";
 import "./about.styles.css";
+import Profiles from './Profiles';
 
 const About = () => {
     return (
         <Container className="aboutContainer">
-            <h1 className="about">About Page</h1>
-            <p className="ojective">We aim to deliver exceptional customer service and user experiences. To do this, we developed a system that allows us to create custom-tailored apps and partnerships that fuel successful projects and processes.
+            <p className="objective">
+            Smart Developers is a software development company with an aim to deliver exceptional customer service and user experiences. To do this, we developed a system that allows us to create custom-tailored apps and partnerships that fuel successful projects and processes.
             </p>
+            <Profiles/>
         </Container>
     )
 }
---
+++ src/pages/about.pages/profiles.obj.js
@@ -0,0 +1,37 @@
+import mobilTilbud from "../../images/mobilTilbud.png";
+import amjadIftikhar from "../../images/amjadIftikhar.jpg";
+import pkInsaf from "../../images/pkInsaf.png";
+import pakSiasat from "../../images/pakSiasat.png";
+import amgsac from "../../images/amgsac.png";
+import qabros from "../../images/qabros.png";
+import daniaz from "../../images/daniaz.png";
+
+const profilesObj = [
+    {
+        profileImage: mobilTilbud,
+        profileTitle: "Faisal Akhlaq",
+        personalInfo: "Backend Developer"
+    },
+    {
+        profileImage: amjadIftikhar,
+        profileTitle: "Amjad Iftikhar",
+        personalInfo: "Frontend Developer"
+    },
+    {
+        profileImage: pkInsaf,
+        profileTitle: "Raja Ejaz",
+        personalInfo: "Full Stack Developer"
+    },
+    {
+        profileImage: pakSiasat,
+        profileTitle: "Amir Mehmood",
+        personalInfo: "Full Stack Developer"
+    },
+    {
+        profileImage: amgsac,
+        profileTitle: "AMGSAC",
+        personalInfo: "Designer"
+    }
+];
+
+export default profilesObj;
