32001efbe50280aaadb3a04b5f7b150ff886f659
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2020-12-30 00:21:00 +0100

    projects' section updated

--- src/components/projects.components/Projects.js
+++ src/components/projects.components/Projects.js
@@ -5,18 +5,23 @@ import "./projects.styles.css";
 
 function Projects() {
     return (
-        <div className="projectSection">
-            {
-            projectsObj.map((p, index) => {
-                return (
-                    <ProjectCard key={index}
-                    projectImage={p.projectImage}
-                    projectTitle={p.projectTitle}
-                    description={p.description} 
-                    />
-                )
-            })
-            }
+        <div className="projectsContainer">
+            <div className="caseStudyTitle">
+                <p className="caseStudyDescription">OUR PROJECTS</p>
+            </div>
+            <div className="projectSection">
+                {
+                projectsObj.map((p, index) => {
+                    return (
+                        <ProjectCard key={index}
+                        projectImage={p.projectImage}
+                        projectTitle={p.projectTitle}
+                        description={p.description} 
+                        />
+                    )
+                })
+                }
+            </div>
         </div>
     )
 }
