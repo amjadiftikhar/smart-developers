d4548f8f477fe5504ec8e67355e49f6cf1928181
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-01 15:57:14 +0100

    changes made in footer section

--- src/components/footer.components/Footer.js
+++ src/components/footer.components/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Projects from "../projects.components/Projects";
+import {FaFacebookF} from "react-icons/fa";
+import {FaLinkedinIn} from "react-icons/fa";
+import {FaTwitter} from "react-icons/fa";
+import {FaInstagramSquare} from "react-icons/fa";
 import "./footer.styles.css"
 
 function Footer() {    
@@ -11,32 +15,45 @@ function Footer() {
             <div className="footerSection">
                 <div className="footerAboutUs">
                     <h3 className="footerHeader">About</h3>
-                    <a className="footerLinks" href="" onClick={goToPortfolioSection}>Portfolio</a>
+                    <a className="footerLinks" href="" 
+                        onClick={goToPortfolioSection}>
+                        Portfolio
+                    </a>
                     <a className="footerLinks" href="/about">Team</a>
                     <a className="footerLinks" href="/contact">Contact</a>
                 </div> 
-                <div className="socialMeadia">
-                    <h3 className="footerHeader">Social Media</h3>               
-                    <a className="footerLinks" href="#">Facebook</a>
-                    <a className="footerLinks" href="#">Linkedin</a>
-                    <a className="footerLinks" href="#">Twitter</a>
-                    <a className="footerLinks" href="#">Instagram</a>                
-                </div>
                 <div className="footerServices">
                     <h3 className="footerHeader">Services</h3>
                     <div>Software Development</div>
                     <div>Custom Software</div>
-                    <div>Full Stack Development</div>
+                    {/* <div>Full Stack Development</div> */}
                     <div>Mobile App Development</div>
-                    <div>CMS</div>
+                    {/* <div>CMS</div> */}
                     <div>E-commerce</div>
-                    <div>Maintenance & Support</div>
+                    {/* <div>Maintenance & Support</div> */}
                     <div>IT Support</div>
+                </div>
+                <div className="socialMeadia">
+                    <h3 className="footerHeader">Social Media</h3> 
+                    <div className="socialMeadiaLinks">              
+                        <a className="footerLinks" href="#">
+                            <FaFacebookF className="socialMediaIcon"/>
+                        </a>
+                        <a className="footerLinks" href="#">
+                            <FaLinkedinIn className="socialMediaIcon"/>
+                        </a>
+                        <a className="footerLinks" href="#">
+                            <FaTwitter className="socialMediaIcon"/>
+                        </a>
+                        <a className="footerLinks" href="#">
+                            <FaInstagramSquare className="socialMediaIcon"/>
+                        </a> 
+                    </div>              
                 </div>
             </div>
             <div className="copyRightSection">
                 <span>
-                    copyright@2020 | All rights reserved
+                    copyright@2021 | All rights reserved
                 </span>
                 <span>Privacy Plocy</span>
             </div>
