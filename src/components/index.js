53f94aaf8eb148dba4d3fd475d53d9371f069210
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-02 19:54:13 +0100

    ServicesPage component added in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import GetQuote from "../pages/getQuote.pages/GetQuote";
 import Layout from "./layout.components/Layout";
 import NavigationBar from "./navigation.components/NavigationBar";
 import Portfolio from "../pages/portfolio.pages/Portfolio";
-import Services from "../pages/services.pages/Services";
+import Services from "../pages/services.pages/ServicesPage";
 import Footer from "./footer.components/Footer";
 
 export {Home, About, Contact, GetQuote, Layout, 
