ed677decfa9b66073cb329f87bf0dd66e670e9ff
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-02 19:58:51 +0100

    Projects component called in portfolio page

--- src/pages/portfolio.pages/Portfolio.js
+++ src/pages/portfolio.pages/Portfolio.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import Projects from '../../components/projects.components/Projects';
 import "./portfolio.styles.css";
 
 const Portfolio = () => {
     return (
-        <Container className="portfolioContainer">
-            <h1>Portfolio Page</h1>
-        </Container>
+        <div className="portfolioContainer">
+            <Projects/>
+        </div>
     )
 }
 
