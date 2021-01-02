df8f633fc400326650fcf5e958e12dce07c8bc7c
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-02 19:58:02 +0100

    name modified to ServicesPage.js

---
+++ src/pages/services.pages/ServicesPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Services from "../../components/services.components/Services";
+import "./servicesPage.styles.css";
+
+const ServicesPage = () => {
+    return (
+        <div className="servicesPageSection">
+            <Services/>
+        </div>
+    )
+}
+
+export default ServicesPage
