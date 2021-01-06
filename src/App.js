60c5df56870064d015db48119b259df31390d952
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-06 10:47:17 +0100

    code clean in App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import {NavigationBar, Home, About, Contact, GetQuote, 
+import {NavigationBar, Home, About, Contact, 
   Layout, Portfolio, Services, Footer} from "./components"
 import './App.css';
 
 function App() {
+
   return (
-    <div>
-      <React.Fragment>
-          <Router>
-          <NavigationBar/>
+    <React.Fragment>
+      <Router>
+        <NavigationBar/>
         <Layout style={{display:"flex", flexDirection:"column"}} fluid={true}>
-            <Switch>
-              <Route exact={true} path="/"  component={Home} />
-              <Route exact={true} path="/about"  component={About} />
-              <Route exact={true} path="/services"  component={Services} />
-              <Route exact={true} path="/portfolio"  component={Portfolio} />
-              <Route exact={true} path="/contact"  component={Contact} />
-              {/* <Route exact={true} path="/getQuote"  component={GetQuote} /> */}
-            </Switch>
+          <Switch>
+            <Route exact={true} path="/"  component={Home} />
+            <Route exact={true} path="/about"  component={About} />
+            <Route exact={true} path="/services"  component={Services} />
+            <Route exact={true} path="/portfolio"  component={Portfolio} />
+            <Route exact={true} path="/contact"  component={Contact} />
+          </Switch>
         </Layout>
-          </Router>
-        <Footer/>        
+      </Router>
+      <Footer/>        
     </React.Fragment>
-    </div>
   );
 }
 
