2e73e2abab94193e3b1973d378fe8778dd385550
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-08 03:36:56 +0100

    spelling check conducted

--- src/pages/about.pages/About.js
+++ src/pages/about.pages/About.js
@@ -7,7 +7,7 @@ const About = () => {
     return (
         <Container className="aboutContainer">
             <p className="objective">
-            Smart Developers is a software development company with an aim to deliver exceptional customer service and user experiences. To do this, we developed a system that allows us to create custom-tailored apps and partnerships that fuel successful projects and processes.
+            Smart Developer is a software development company with an aim to deliver exceptional customer service and user experiences. To do this, we developed a system that allows us to create custom-tailored apps and partnerships that fuel successful projects and processes.
             </p>
             <Profiles/>
         </Container>
