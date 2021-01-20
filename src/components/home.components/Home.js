bcc3a91a0e7f8272b99a5a49bc1999ede78e8b5a
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-20 23:35:07 +0100

    change made in Home.js

--- src/components/home.components/Home.js
+++ src/components/home.components/Home.js
@@ -14,8 +14,8 @@ const Home  = () => {
             <Services/> 
             <ScrollToDown/>
             <ScrollToTop/>
-            <Tools/>
             <Projects/>
+            <Tools/>
         </div>
     )
 }
