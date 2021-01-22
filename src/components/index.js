f4cf5a14d9364b3dd9cf1f1702b70cb9d6e7bf8d
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-22 23:37:59 +0100

    comments added in index file

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+// importing all routes in this index file
 import Home from "./home.components/Home";
 import About from "../pages/about.pages/About";
 import Contact from "../pages/contact.pages/Contact";
@@ -8,6 +9,7 @@ import Portfolio from "../pages/portfolio.pages/Portfolio";
 import Services from "../pages/services.pages/ServicesPage";
 import Footer from "./footer.components/Footer";
 
+// exporting all routes from this index file
 export {Home, About, Contact, GetQuote, Layout, 
     NavigationBar, Portfolio, Services, Footer
 };
