88ef51f0f667da8d35e1d1cf995736169f3cc6dc
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-22 23:39:20 +0100

    comments added in technologies section

--- src/components/technologies.components/ToolCard.js
+++ src/components/technologies.components/ToolCard.js
@@ -1,16 +1,22 @@
 import React, {useEffect} from 'react';
+
 import Aos from "aos";
 import "aos/dist/aos.css";
 
 function Tool(props) {
+
     useEffect(() => {
         Aos.init({duration: 2000});
     }, [])
+
     return (
-        <div className="toolCard" data-aos="fade-left">
-            <img className="toolImage" src={props.toolImage} alt=""/>
-            <span className="toolName">{props.toolName}</span>
-        </div>
+        <>
+            {/* data-aos, applying animation on the section */}
+            <div className="toolCard" data-aos="fade-left">
+                <img className="toolImage" src={props.toolImage} alt=""/>
+                <span className="toolName">{props.toolName}</span>
+            </div>
+        </>
     )
 }
 
