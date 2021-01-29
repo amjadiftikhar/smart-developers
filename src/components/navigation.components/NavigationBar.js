7b3e16b732d6d0a8671623421560130242359966
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-29 01:17:58 +0100

    smart changes made in home styles and navigationBar.js

--- src/components/navigation.components/NavigationBar.js
+++ src/components/navigation.components/NavigationBar.js
@@ -26,7 +26,6 @@ const Header = (props) => {
           <LinkContainer to="/services"><NavLink>Services</NavLink></LinkContainer>
           <LinkContainer to="/portfolio"><NavLink>Portfolio</NavLink></LinkContainer>
           <LinkContainer to="/contact"><NavLink>Contact</NavLink></LinkContainer>
-          {/* <LinkContainer to="/getQuote"><NavLink>Get a quote</NavLink></LinkContainer> */}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
