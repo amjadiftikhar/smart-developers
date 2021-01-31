81a78b41a979a0024b94e1329aad5b7a664a88ab
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-01-31 15:49:53 +0100

    pageNotfound component added

---
+++ src/pages/pageNotFound/PagenotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+
+import "./pageNotFound.styles.css";
+
+function PagenotFound() {
+    return (
+        <Container className="pageNotFoundContainer">
+            <div className="errorText">
+                <h1>Ooops!.. </h1>
+                <p>We can&apos;t seem to find the page you&apos;re looking for.</p>
+                <p>Error code: 404</p>
+                <p>Try to reach out using the menu on the top! </p>
+            </div>
+        </Container>
+    )
+}
+
+export default PagenotFound
