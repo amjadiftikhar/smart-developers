7f2befc8612917a4ca41023a8e4882e5c2a467b8
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-02-03 22:16:46 +0100

    getQuote component removed

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@
 import Home from "./home.components/Home";
 import About from "../pages/about.pages/About";
 import Contact from "../pages/contact.pages/Contact";
-import GetQuote from "../pages/getQuote.pages/GetQuote";
 import Layout from "./layout.components/Layout";
 import NavigationBar from "./navigation.components/NavigationBar";
 import Portfolio from "../pages/portfolio.pages/Portfolio";
@@ -11,6 +10,6 @@ import Footer from "./footer.components/Footer";
 import Page404 from "../pages/pageNotFound/PageNotFound";
 
 // exporting all routes from this index file
-export {Home, About, Contact, GetQuote, Layout, 
+export {Home, About, Contact, Layout, 
     NavigationBar, Portfolio, Services, Footer, Page404
 };
