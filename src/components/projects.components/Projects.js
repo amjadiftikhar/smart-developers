14c5403f9d04ac1674efcf4f2099d2b9b2aeeccc
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-02-11 01:30:54 +0100

    ProjectCard moved in components and other files updated

--- src/components/projects.components/Projects.js
+++ src/components/projects.components/Projects.js
@@ -1,30 +1,22 @@
 import React, {useRef, useState} from 'react';
 import projectsObj from "./projects.obj";
-import ProjectCard from "./ProjectCard";
+import ProjectCard from "./components/ProjectCard";
 import "./projects.styles.css";
-import Hello from '../projectDescription.components/Hello';
-import Hello2 from '../projectDescription.components/Hello2';
-import Hello3 from '../projectDescription.components/Hello3';
+import ProjectModal from './components/ProjectModal';
 
 function Projects() {  
 
    const portfolioSection = useRef(null)
-   const [toggleHello, setToggleHello] = useState(0) 
-   const [toggleHello1, setToggleHello1] = useState(0)
-   const [toggleHello2, setToggleHello2] = useState(0) 
+   const [activeProject, setActiveProject] = useState('') 
+   const [showActiveProject, setShowActiveProject] = useState(false)
 
-   const onClick = (id) => {
-       console.log("ID of project", id)
-    //    if(id==='0') {
-    //    setToggleHello(1)
-    // }
-    // else if (id='1'){
-    //     setToggleHello1(1)
-    // }else if (id='2') {
-    //     setToggleHello2(1)
-    // }else{
-    //     setToggleHello(0)
-    // }
+   const showModal = (projectObject) => {
+        setActiveProject(projectObject)
+        setShowActiveProject(true)
+   }
+
+   const hideModal = () => {
+       setShowActiveProject(false)
    }
 
     return (
@@ -33,23 +25,24 @@ function Projects() {
                 <h2 className="caseStudyDescription">OUR PROJECTS</h2>
             </div>
             <div className="projectSection" ref={portfolioSection}>
-                { toggleHello ? <Hello/> : toggleHello1 ? <Hello2/> : toggleHello2 ? <Hello3/> :
+                { 
                     projectsObj.map((p, index) => {
-
                         return (
                             <ProjectCard key={index}
-                                onClick={onClick}
+                                projectObject={p}
+                                onClick={showModal}
                                 id={p.id}
                                 projectImage={p.projectImage}
                                 projectTitle={p.projectTitle}
                                 description={p.description} 
                             />
                         )
-                    }) 
+                    })   
+                        
                 }
-            </div>
-        </div>
-    )
+            </div>  {showActiveProject  && <ProjectModal project={activeProject} handleClose={hideModal} />}
+        </div>  
+    ) 
 }
 
 export default Projects;
