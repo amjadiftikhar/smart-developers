23a8301f8c359be94a955fd844c06681e288dbee
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-02-11 01:29:32 +0100

    components created in components directory

---
+++ src/components/projects.components/components/ProjectCard.js
@@ -0,0 +1,46 @@
+import React, {useEffect, useState} from 'react';
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+function ProjectCard(props) {
+    const [projDescription, toggleProjDescription] = useState(false)
+    const [hide, setHide] = useState(true)
+
+    useEffect(() => {
+        Aos.init({duration: 2000});        
+    }, [projDescription])
+
+    // function to set toggle states
+    function handleFnc(e){
+        e.preventDefault();
+        toggleProjDescription(projDescription => !projDescription);
+        setHide (hide => !hide )    
+    }
+    return (
+        <>
+            {/* data-aos, applying animation on the section */}
+            <div className="projectCard" data-aos="fade-up"  
+                onClick={() => props.onClick(props.projectObject)} >
+                {/* <div className="projDescriptionSection">
+                    <h4 className="projectTitle">
+                        {props.projectTitle}
+                    </h4>
+                    <p className="projectDescription">
+                        {projDescription && props.description}
+                    </p>
+                    <a className="showDescription" 
+                        href="" onClick={handleFnc}>
+                        {hide ? 'more details...' : 'hide details >>'}                
+                    </a>
+                </div> */}
+                <div className="projImageSection">
+                    <img className="projectImage" 
+                        src={props.projectImage} alt=""
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProjectCard;
