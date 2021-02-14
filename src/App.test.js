955c02eacb65437d1056ffd3e9d5d47b40e297da
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-02-14 23:51:42 +0100

    test updated for App

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/app/);
+  const {queryByText} = render(<App />);
+  const linkElement = queryByText(/app/i);
   expect(linkElement).toBeInTheDocument();
 });
