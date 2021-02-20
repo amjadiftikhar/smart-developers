e07c0c323dc92de8e625a1081d98815aa93c06fe
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-02-20 13:34:20 +0100

    code cleaned

--- src/components/popup.components/PopUp.js
+++ src/components/popup.components/PopUp.js
@@ -1,7 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import { useHistory } from 'react-router-dom';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
 import './popup.styles.css';
 
 
@@ -20,19 +18,10 @@ export default function Popup() {
         }, 3000);
     }, [])
 
-    // Aos.init({duration: 2000});
-    // useEffect(()=>{
-    //   let pop_status = localStorage.getItem('pop_status');
-    //   if(!pop_status){
-    //     setVisible(true);
-    //     localStorage.setItem('pop_status', 30);
-    //   }
-    // },[])
-
     if(!showBubble) return null;
 
     return (
-        <div className='popUpInner'data-aos='fade-up'>
+        <div className='popUpInner'>
             {/* onClick redirecting to contact page */}
             <p className='speechBubble' onClick={callContactForm}>
               ?
