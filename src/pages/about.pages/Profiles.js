8c321cf2b8934db89f6ff79fc885224148c63cd3
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-02-22 00:48:54 +0100

    profiles object replaced with json

--- src/pages/about.pages/Profiles.js
+++ src/pages/about.pages/Profiles.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import profilesObj from "./profiles.obj";
+import profileData from "../../assets/data.json";
 import {Container} from "react-bootstrap";
 import ProfileCard from "./ProfileCard";
 
@@ -9,7 +9,8 @@ function Profiles() {
             <h1 className="sectionTitle">MEET OUR TEAM</h1>
             <div className="profilesContainer">
                 {
-                    profilesObj.map((p, index) => {
+                    // map through profile data from json
+                    profileData.profile.map((p, index) => {
                         return(
                             <ProfileCard
                                 key = {index} 
--- src/pages/about.pages/profiles.obj.js
+++ src/pages/about.pages/profiles.obj.js
@@ -1,35 +1,35 @@
-import faisalAkhlaq from "../../images/faisalAkhlaq.png";
-import amjadIftikhar from "../../images/amjadIftikhar.jpg";
-import rajaEjaz from "../../images/rajaEjaz.jpg";
-import rahim from "../../images/rahim.jpeg";
-import amirMajeed from "../../images/amirMajeed.JPG";
+// import faisalAkhlaq from "../../images/faisalAkhlaq.png";
+// import amjadIftikhar from "../../images/amjadIftikhar.jpg";
+// import rajaEjaz from "../../images/rajaEjaz.jpg";
+// import rahim from "../../images/rahim.jpeg";
+// import amirMajeed from "../../images/amirMajeed.JPG";
 
-const profilesObj = [
-    {
-        profileImage: faisalAkhlaq,
-        profileTitle: "Faisal Akhlaq",
-        personalInfo: "Backend Developer"
-    },
-    {
-        profileImage: amjadIftikhar,
-        profileTitle: "Amjad Iftikhar",
-        personalInfo: "Frontend Developer"
-    },
-    {
-        profileImage: rahim,
-        profileTitle: "Rahim Ismaili",
-        personalInfo: "Full Stack Developer"
-    },
-    {
-        profileImage: rajaEjaz,
-        profileTitle: "Raja Ejaz",
-        personalInfo: "Full Stack Developer"
-    },
-    {
-        profileImage: amirMajeed,
-        profileTitle: "Amir Mehmood",
-        personalInfo: "Full Stack Developer"
-    }
-];
+// const profilesObj = [
+//     {
+//         profileImage: faisalAkhlaq,
+//         profileTitle: "Faisal Akhlaq",
+//         personalInfo: "Backend Developer"
+//     },
+//     {
+//         profileImage: amjadIftikhar,
+//         profileTitle: "Amjad Iftikhar",
+//         personalInfo: "Frontend Developer"
+//     },
+//     {
+//         profileImage: rahim,
+//         profileTitle: "Rahim Ismaili",
+//         personalInfo: "Full Stack Developer"
+//     },
+//     {
+//         profileImage: rajaEjaz,
+//         profileTitle: "Raja Ejaz",
+//         personalInfo: "Full Stack Developer"
+//     },
+//     {
+//         profileImage: amirMajeed,
+//         profileTitle: "Amir Mehmood",
+//         personalInfo: "Full Stack Developer"
+//     }
+// ];
 
-export default profilesObj;
+// export default profilesObj;
