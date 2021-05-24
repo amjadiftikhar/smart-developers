2dfc6ebd1caa62ab388e4965651c70c24a61f82a
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-05-24 12:11:57 +0200

    changes made

--- src/components/services.components/PortfolioCatalog.js
+++ src/components/services.components/PortfolioCatalog.js
@@ -1,42 +1,54 @@
 import {GiPriceTag} from "react-icons/gi";
 import {FaExternalLinkSquareAlt} from "react-icons/fa";
 import portfolioCatalogData from "../../assets/portfolioCatalogData.json";
-import "../projects.components/projects.styles.css";
+// import "../projects.components/projects.styles.css";
 import "./portfoliocatalog.styles.css"
 import React from "react";
 
 function PortfolioCatalog() {
     return (
-        <div className="projectsContainer" data-testid='projectId' >
-            <div className="pc-title">
+        <div className="catalogMainContainer" data-testid='projectId' >
+            <div className="pCatalogTitle">
                 <h2 className="caseStudyDescription">Portfolio Catalog</h2>
             </div>
-            <div className="pcDescription">
+            <div className="catalogDescription">
                 <p>Boost your business and career by choosing 
                     a portfolio for yourself. We do everything for you, 
                     from filling your data in the choosen portfolio to
                     customizing it and setting it up on the internet.
                 </p>
-                <p><b>What I get in price</b>
+                {/* <p><b>What I get in price</b>
                     <ul>
                         <li>A brand new portfolio</li>
                         <li>A domain with your own name of choice</li>
                         <li>Your portfolio hosting</li>
                         All in one price.
                     </ul>
-                </p>
+                </p> */}
+                
             </div>
-            <div className="pcContainer ">
+
+            <div className="offerSection">
+                <h3 className="offerTitle"><b>What we get in price</b></h3>
+                <ul>
+                    <li>A brand new portfolio</li>
+                    <li>A domain with your own name of choice</li>
+                    <li>Your portfolio hosting</li>
+                    {/* All in one price. */}
+                </ul>
+            </div>
+
+            <div className="catalogContainer ">
                 {
                     portfolioCatalogData.portfolioCatalog.map((proj, index) => {
                         return (
                             // Play the portfolio video
-                            <div key={index} className="pcIndividual">
-                                <video className="pcVideo" 
+                            <div key={index} className="catalogCard">
+                                <video className="catalogVideo" 
                                     controls loop autoPlay muted >
                                         <source src={proj.video} />
                                 </video>
-                                <div className="pcPriceAndLink ">
+                                <div className="catalogPriceAndLink ">
                                     <span><GiPriceTag style={{marginLeft:'.8em', marginRight:'.5em'}} /><b>Price</b>: {proj.price}</span>
                                     <a href={proj.link} target="blank" className="pcLinkBtn"><FaExternalLinkSquareAlt style={{marginRight:'.5em'}} />Live Demo Link</a>
                                 </div>
